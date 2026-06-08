6e72f5fcfdad97f5e2e1713385be6b3f80231468
devilsinthedeetz <andrew@devilsinthedeetz.us>
2026-06-07 20:15:18 -0600

    pass in prompt as an argument

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
-
-if api_key is None:
-    raise RuntimeError("No api key present in .env")
-
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environmental variable not set")
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+        contents=messages
     )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed.")
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
 
 
