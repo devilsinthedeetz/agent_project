ea7ba27a643ac485a4e8cfc2d649d9974ef1ef3e
devilsinthedeetz <andrew@devilsinthedeetz.us>
2026-06-09 00:27:00 -0600

    get_files_info now returns minor file details

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,13 +2,28 @@ import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
-        working_dir_abs = os.path.abspath(working_directory)
-        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-        valid_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        working_dir_abs: str = os.path.abspath(working_directory)
+        target_dir: str = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_dir: bool = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
         if not valid_dir:
             raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-        if not os.path.isdir(directory):
+        if not os.path.isdir(target_dir):
             raise Exception(f'Error: "{directory}" is not a directory')
-        return f'Success: "{directory}" is within the working directory'
+
+        def files_info(target_dir_files: list[str], target_dir: str) -> str:
+            file_strings: list[str] = []
+            for file in target_dir_files:
+                file_path: str = os.path.normpath(os.path.join(target_dir, file))
+                file_strings.append(f"  - {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
+            return '\n'.join(file_strings)
+
+        def dir_desc(directory: str, target_dir: str, working_dir_abs: str) -> str:
+            if target_dir == working_dir_abs:
+                return "current directory"
+            else:
+                return f"'{directory}'"
+
+
+        return f"Results for {dir_desc(directory, target_dir, working_dir_abs)} directory: \n" + files_info(os.listdir(target_dir), target_dir)
     except Exception as e:
         return e
