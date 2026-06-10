1f8dbe4b02f202e1686a6fc57ab1dbee78b2b10f
devilsinthedeetz <andrew@devilsinthedeetz.us>
2026-06-10 00:52:23 -0600

    write_files function complete

--- functions/write_file.py
+++ functions/write_file.py
@@ -9,6 +9,9 @@ def write_file(working_directory: str, file_path: str, content: str) -> str:
             raise Exception(f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
         if os.path.isdir(target_file):
             raise Exception(f'Error: Cannot write to "{file_path}" as it is a directory')
-        print(f"{target_file}")
+        os.makedirs(os.path.dirname(target_file), exist_ok=True)
+        with open(target_file, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return e
