6a6ae95211d39292b65aa7ec699e91ae0015ea06
devilsinthedeetz <andrew@devilsinthedeetz.us>
2026-06-09 21:41:41 -0600

    get_file_contents.py returns file contents, truncated at the MAX_CHARS parameter in config.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,19 @@
 import os
+from config import MAX_CHARS
 
 def get_file_content(working_directory: str, file_path: str) -> str:
-    pass
+    try:
+        working_dir_abs: str = os.path.abspath(working_directory)
+        target_file: str = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_file: bool = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+        if not valid_file:
+            raise Exception(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+        if not os.path.isfile(target_file):
+            raise Exception(f'Error: File not found or is not a regular file: "{file_path}"')
+        with open(target_file, "r") as f:
+            content = f.read(MAX_CHARS)
+            if f.read(1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            return content
+    except Exception as e:
+        return e
